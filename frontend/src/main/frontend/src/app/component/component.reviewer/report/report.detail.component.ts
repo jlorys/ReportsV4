7564ff84cd19cdd8165b02b0e116ac2095bb3d18
Kuba <kuba35@vp.pl>
2017-09-12 19:02:49 +0200

    refactor

--- frontend/src/main/frontend/src/app/component/component.reviewer/report/report.detail.component.ts
+++ frontend/src/main/frontend/src/app/component/component.reviewer/report/report.detail.component.ts
@@ -1,13 +1,10 @@
 import {Component, Input, OnDestroy} from "@angular/core";
-import {ActivatedRoute, Router} from "@angular/router";
+import {ActivatedRoute} from "@angular/router";
 import {Message, SelectItem} from "primeng/primeng";
 import {ReviewerReportDataService} from "./report.data.service";
 import {saveAs as importedSaveAs} from "file-saver";
 import {Report} from "../../component.admin/report/report";
-import {AppUser} from "../../component.admin/users/user";
 import {Laboratory} from "../../component.admin/laboratory/laboratory";
-import {AppUserDataService} from "../../component.admin/users/user.data.service";
-import {LaboratoryDataService} from "../../component.admin/laboratory/laboratory.data.service";
 
 @Component({
   selector: 'reviewerReports-detail',
@@ -17,20 +14,17 @@ export class ReviewerReportsDetailComponent implements OnDestroy {
 
   @Input() header = "Sprawozdania użytkownika...";
   report: Report;
-  sourceUsers: AppUser[] = [];
 
   private params_subscription: any;
 
   msgs: Message[] = [];
-  filePath: string = "/uploaded_files";
 
   laboratory: Laboratory;
-  sourceLaboratoriesSelectItems: SelectItem[];
 
   grade: string;
   grades: SelectItem[];
 
-  constructor(private route: ActivatedRoute, private router: Router, private reviewerReportDataService: ReviewerReportDataService, private appUserDataService: AppUserDataService, private laboratoryDataService: LaboratoryDataService) {
+  constructor(private route: ActivatedRoute, private reviewerReportDataService: ReviewerReportDataService) {
     this.grades = [];
     this.grades.push({label:'2', value:2});
     this.grades.push({label:'2.5', value:2.5});
@@ -40,45 +34,18 @@ export class ReviewerReportsDetailComponent implements OnDestroy {
     this.grades.push({label:'4.5', value:4.5});
     this.grades.push({label:'5', value:5});
 
-    this.sourceLaboratoriesSelectItems = [];
-    this.sourceLaboratoriesSelectItems.push({label: '--------------------------', value: null});
-
-    laboratoryDataService.findAll().subscribe(subject => subject.forEach((value, index, array) => this.sourceLaboratoriesSelectItems.push({
-        label: value.name + " " + value.description + " " + new Date(value.labDate).toString(),
-        value: value
-      })),
-      error => this.msgs.push({
-        severity: 'error',
-        summary: 'Constructor laboratory of report error',
-        detail: error
-      }));
-
     this.params_subscription = this.route.params.subscribe(params => {
       let id = params['id'];
-      console.log('Constructor for reports-add ' + id);
-      if (id === 'add') {
-        this.report = new Report();
-        this.laboratory = null;
-        this.grade = null;
+      console.log('Constructor for reviewerReports-detail ' + id);
 
-        appUserDataService.findAllAppUsersWhichDoNotHaveReportWithThisId(0).//0 means find all
-        subscribe(users => this.sourceUsers = users,
-          error => this.msgs.push({severity: 'error', summary: 'Constructor user reports error', detail: error}))
-
-      } else {
         this.reviewerReportDataService.getReport(id)
           .subscribe(report => {
               this.report = report;
               this.laboratory = report.laboratory;
               this.grade = report.grade;
-
-              appUserDataService.findAllAppUsersWhichDoNotHaveReportWithThisId(this.report.id).subscribe(users => this.sourceUsers = users,
-                error => this.msgs.push({severity: 'error', summary: 'Constructor user reports error', detail: error}))
-
             },
             error => this.msgs.push({severity: 'error', summary: 'Constructor error', detail: error})
           );
-      }
     });
   }
 
@@ -95,29 +62,6 @@ export class ReviewerReportsDetailComponent implements OnDestroy {
     this.reviewerReportDataService.update(this.report).subscribe(
       report => {
         this.report = report;
-        this.msgs.push({severity: 'info', summary: 'Zapisano', detail: 'OK!'})
-      },
-      error => this.msgs.push({severity: 'error', summary: 'Nie można zapisać', detail: error})
-    );
-  }
-
-  myUploader(event) {
-    this.msgs = []; //this line fix disappearing of messages
-
-    this.report.filePath = this.filePath;
-    let fullFileName = event.files[0].name;
-    this.report.fileExtension = '.' + fullFileName.split('.').pop();
-    this.report.fileName = fullFileName.split('.').shift();
-
-    this.reviewerReportDataService.update(this.report).subscribe(
-      report => {
-        this.report = report;
-
-        let xhr = new XMLHttpRequest(), formData = new FormData();
-        formData.append("file", event.files[0], this.report.id + "_r_" + fullFileName);
-        xhr.open("POST", '/api/reports/upload', true);
-        xhr.send(formData);
-
         this.msgs.push({severity: 'info', summary: 'Zapisano', detail: 'OK!'})
       },
       error => this.msgs.push({severity: 'error', summary: 'Nie można zapisać', detail: error})
